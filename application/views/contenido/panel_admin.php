bb0d6327a6b54fe94d851a8aad190df84ad54b1c
Julio Vinachi <julio899@gmail.com>
2015-06-11 11:58:15 -0430

    traduciendo .js de tabla

--- application/views/contenido/panel_admin.php
+++ application/views/contenido/panel_admin.php
@@ -115,12 +115,9 @@
             <div class="collapse navbar-collapse navbar-ex1-collapse">
                 <ul class="nav navbar-nav side-nav">
                     <li class="active">
-                        <a href="index.html"><i class="fa fa-fw fa-dashboard"></i> Escritorio</a>
+                        <a href="<?php echo base_url().index_page();?>/administrador"><i class="fa fa-fw fa-dashboard"></i> Escritorio</a>
                     </li>
 
-                    <li>
-                        <a href="blank-page.html"><i class="fa fa-fw fa-file"></i> Crear Producto</a>
-                    </li>
 
                     <li>
                         <a href="charts.html"><i class="fa fa-fw fa-user"></i> Registrar Cliente</a>
@@ -409,59 +406,12 @@
                                             </tr>
                                         </thead>
                                         <tbody>
-                                            <tr>
-                                                <td>3326</td>
-                                                <td>10/21/2013</td>
-                                                <td>3:29 PM</td>
-                                                <td>$321.33</td>
-                                            </tr>
-                                            <tr>
-                                                <td>3325</td>
-                                                <td>10/21/2013</td>
-                                                <td>3:20 PM</td>
-                                                <td>$234.34</td>
-                                            </tr>
-                                            <tr>
-                                                <td>3324</td>
-                                                <td>10/21/2013</td>
-                                                <td>3:03 PM</td>
-                                                <td>$724.17</td>
-                                            </tr>
-                                            <tr>
-                                                <td>3323</td>
-                                                <td>10/21/2013</td>
-                                                <td>3:00 PM</td>
-                                                <td>$23.71</td>
-                                            </tr>
-                                            <tr>
-                                                <td>3322</td>
-                                                <td>10/21/2013</td>
-                                                <td>2:49 PM</td>
-                                                <td>$8345.23</td>
-                                            </tr>
-                                            <tr>
-                                                <td>3321</td>
-                                                <td>10/21/2013</td>
-                                                <td>2:23 PM</td>
-                                                <td>$245.12</td>
-                                            </tr>
-                                            <tr>
-                                                <td>3320</td>
-                                                <td>10/21/2013</td>
-                                                <td>2:15 PM</td>
-                                                <td>$5663.54</td>
-                                            </tr>
-                                            <tr>
-                                                <td>3319</td>
-                                                <td>10/21/2013</td>
-                                                <td>2:13 PM</td>
-                                                <td>$943.45</td>
-                                            </tr>
+
                                         </tbody>
                                     </table>
                                 </div>
                                 <div class="text-right">
-                                    <a href="#">View All Transactions <i class="fa fa-arrow-circle-right"></i></a>
+                                    <a href="#">Ver Todas las Transacciones <i class="fa fa-arrow-circle-right"></i></a>
                                 </div>
                             </div>
                         </div>
