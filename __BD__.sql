7eef161fb52779dd3a014a15725065b5ec11c009
Julio Vinachi <julio899>
2015-10-28 21:33:28 -0430

    agregando campos en tabla compras

--- __BD__.sql
+++ __BD__.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.2.11
+-- version 4.0.10deb1
 -- http://www.phpmyadmin.net
 --
 -- Servidor: localhost
--- Tiempo de generación: 24-09-2015 a las 16:04:56
+-- Tiempo de generación: 28-10-2015 a las 21:32:12
 -- Versión del servidor: 5.5.44-0ubuntu0.14.04.1
--- Versión de PHP: 5.5.9-1ubuntu4.12
+-- Versión de PHP: 5.5.9-1ubuntu4.13
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
@@ -19,6 +19,8 @@ SET time_zone = "+00:00";
 --
 -- Base de datos: `inventariomini`
 --
+CREATE DATABASE IF NOT EXISTS `inventariomini` DEFAULT CHARACTER SET utf8 COLLATE utf8_spanish_ci;
+USE `inventariomini`;
 
 -- --------------------------------------------------------
 
@@ -26,11 +28,13 @@ SET time_zone = "+00:00";
 -- Estructura de tabla para la tabla `categorias`
 --
 
-DROP TABLE IF EXISTS `categorias`;
 CREATE TABLE IF NOT EXISTS `categorias` (
-`id` int(11) NOT NULL,
-  `nombre_categoria` varchar(50) COLLATE utf8_spanish_ci NOT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=13 DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `nombre_categoria` varchar(50) COLLATE utf8_spanish_ci NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `nombre_categoria` (`nombre_categoria`),
+  KEY `nombre_categoria_2` (`nombre_categoria`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci AUTO_INCREMENT=13 ;
 
 --
 -- Truncar tablas antes de insertar `categorias`
@@ -58,19 +62,24 @@ INSERT INTO `categorias` (`id`, `nombre_categoria`) VALUES
 -- Estructura de tabla para la tabla `compras`
 --
 
-DROP TABLE IF EXISTS `compras`;
 CREATE TABLE IF NOT EXISTS `compras` (
-`id` int(10) NOT NULL,
+  `id` int(10) NOT NULL AUTO_INCREMENT,
   `idProveedor` int(10) NOT NULL,
   `idU` int(11) NOT NULL,
   `fecha` datetime NOT NULL,
+  `afecta` varchar(10) COLLATE utf8_spanish_ci NOT NULL,
   `monto` varchar(25) COLLATE utf8_spanish_ci NOT NULL,
   `tipo` varchar(1) COLLATE utf8_spanish_ci NOT NULL,
-  `productos` varchar(10000) COLLATE utf8_spanish_ci NOT NULL,
+  `descripcion` varchar(10000) COLLATE utf8_spanish_ci NOT NULL,
   `nro_fac` varchar(15) COLLATE utf8_spanish_ci NOT NULL,
+  `nro_control` varchar(15) COLLATE utf8_spanish_ci NOT NULL,
   `tipo_cuenta` varchar(50) COLLATE utf8_spanish_ci NOT NULL,
-  `status` varchar(1) COLLATE utf8_spanish_ci NOT NULL DEFAULT 'A'
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
+  `status` varchar(1) COLLATE utf8_spanish_ci NOT NULL DEFAULT 'A',
+  PRIMARY KEY (`id`),
+  KEY `idProveedor` (`idProveedor`),
+  KEY `idU` (`idU`),
+  KEY `tipo_cuenta` (`tipo_cuenta`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci AUTO_INCREMENT=4 ;
 
 --
 -- Truncar tablas antes de insertar `compras`
@@ -81,8 +90,8 @@ TRUNCATE TABLE `compras`;
 -- Volcado de datos para la tabla `compras`
 --
 
-INSERT INTO `compras` (`id`, `idProveedor`, `idU`, `fecha`, `monto`, `tipo`, `productos`, `nro_fac`, `tipo_cuenta`, `status`) VALUES
-(3, 2, 0, '2015-06-25 02:06:20', '2000', 'G', '{"motivo":"Gastos"}', '', '', 'A');
+INSERT INTO `compras` (`id`, `idProveedor`, `idU`, `fecha`, `afecta`, `monto`, `tipo`, `descripcion`, `nro_fac`, `nro_control`, `tipo_cuenta`, `status`) VALUES
+(3, 2, 0, '2015-06-25 02:06:20', '', '2000', 'G', '{"motivo":"Gastos"}', '', '', '', 'A');
 
 -- --------------------------------------------------------
 
@@ -90,15 +99,19 @@ INSERT INTO `compras` (`id`, `idProveedor`, `idU`, `fecha`, `monto`, `tipo`, `pr
 -- Estructura de tabla para la tabla `cuentas`
 --
 
-DROP TABLE IF EXISTS `cuentas`;
 CREATE TABLE IF NOT EXISTS `cuentas` (
-`id` int(11) NOT NULL,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `codigo` varchar(25) COLLATE utf8_spanish_ci NOT NULL,
   `nombre` varchar(50) COLLATE utf8_spanish_ci NOT NULL,
   `naturaleza` varchar(25) COLLATE utf8_spanish_ci NOT NULL,
   `status` varchar(1) COLLATE utf8_spanish_ci NOT NULL DEFAULT 'A',
-  `usuario` varchar(25) COLLATE utf8_spanish_ci NOT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
+  `usuario` varchar(25) COLLATE utf8_spanish_ci NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `nombre` (`nombre`),
+  UNIQUE KEY `codigo` (`codigo`),
+  KEY `usuario` (`usuario`),
+  KEY `usuario_2` (`usuario`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci AUTO_INCREMENT=10 ;
 
 --
 -- Truncar tablas antes de insertar `cuentas`
@@ -113,7 +126,8 @@ INSERT INTO `cuentas` (`id`, `codigo`, `nombre`, `naturaleza`, `status`, `usuari
 (5, '10010', 'Gastos Administrativos', 'D', 'A', 'mari'),
 (6, '2020', 'Nomina', 'D', 'A', 'mari'),
 (7, '3030', 'Caja chica', 'H', 'A', 'mari'),
-(8, '123123', 'gastos de servicios', 'D', 'A', 'mari');
+(8, '123123', 'gastos de servicios', 'D', 'A', 'mari'),
+(9, '1414', 'gastos de oficina', 'D', 'A', 'mari');
 
 -- --------------------------------------------------------
 
@@ -121,15 +135,17 @@ INSERT INTO `cuentas` (`id`, `codigo`, `nombre`, `naturaleza`, `status`, `usuari
 -- Estructura de tabla para la tabla `empresas`
 --
 
-DROP TABLE IF EXISTS `empresas`;
 CREATE TABLE IF NOT EXISTS `empresas` (
-`id` int(10) NOT NULL,
+  `id` int(10) NOT NULL AUTO_INCREMENT,
   `codigo` varchar(10) COLLATE utf8_spanish_ci NOT NULL,
   `razon` varchar(100) COLLATE utf8_spanish_ci NOT NULL,
   `rif` varchar(50) COLLATE utf8_spanish_ci NOT NULL,
   `direccion` varchar(500) COLLATE utf8_spanish_ci NOT NULL,
-  `usuario` varchar(50) COLLATE utf8_spanish_ci NOT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
+  `usuario` varchar(50) COLLATE utf8_spanish_ci NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `codigo` (`codigo`),
+  KEY `usuario` (`usuario`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci AUTO_INCREMENT=8 ;
 
 --
 -- Truncar tablas antes de insertar `empresas`
@@ -149,9 +165,8 @@ INSERT INTO `empresas` (`id`, `codigo`, `razon`, `rif`, `direccion`, `usuario`) 
 -- Estructura de tabla para la tabla `productos`
 --
 
-DROP TABLE IF EXISTS `productos`;
 CREATE TABLE IF NOT EXISTS `productos` (
-`id` int(5) NOT NULL,
+  `id` int(5) NOT NULL AUTO_INCREMENT,
   `codigo` varchar(25) COLLATE utf8_spanish_ci NOT NULL,
   `descripcion` varchar(50) COLLATE utf8_spanish_ci NOT NULL,
   `categoria` varchar(20) COLLATE utf8_spanish_ci NOT NULL DEFAULT '0',
@@ -159,8 +174,9 @@ CREATE TABLE IF NOT EXISTS `productos` (
   `venta` varchar(15) COLLATE utf8_spanish_ci NOT NULL,
   `exento` int(1) NOT NULL,
   `status` varchar(1) COLLATE utf8_spanish_ci NOT NULL DEFAULT 'A',
-  `cantidad` int(7) NOT NULL DEFAULT '0'
-) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
+  `cantidad` int(7) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci AUTO_INCREMENT=11 ;
 
 --
 -- Truncar tablas antes de insertar `productos`
@@ -173,14 +189,14 @@ TRUNCATE TABLE `productos`;
 
 INSERT INTO `productos` (`id`, `codigo`, `descripcion`, `categoria`, `compra`, `venta`, `exento`, `status`, `cantidad`) VALUES
 (1, 'frut011', 'melocoton', '10', '435.30', '525.10', 0, 'A', 20),
-(3, 'frut002', 'Patilla Grande', '10', '400.00', '561.22', 0, 'A', 20),
+(3, 'frut015', 'Patilla criolla', '10', '400.00', '561.22', 0, 'A', 20),
 (4, 'frut010', 'aguacate', '10', '105.50', '150.22', 0, 'A', 300),
 (5, 'frut001', 'mango', '10', '100.00', '120.00', 0, 'A', 100),
 (6, 'frut005', 'melon', '10', '100.00', '140', 0, 'A', 5),
 (7, 'frut003', 'guallaba', '10', '390', '420', 0, 'A', 20),
 (8, 'frut002', 'Patilla Grande', '10', '400.00', '561.23', 0, 'A', 20),
 (9, 'frut012', 'mandarina', '10', '40.00', '52.25', 0, 'A', 70),
-(10, '501', 'tamarindo', '5', '10.00', '15.00', 0, 'A', 10);
+(10, '501', 'tamarindo ok', '5', '10.00', '15.00', 0, 'A', 10);
 
 -- --------------------------------------------------------
 
@@ -188,17 +204,18 @@ INSERT INTO `productos` (`id`, `codigo`, `descripcion`, `categoria`, `compra`, `
 -- Estructura de tabla para la tabla `proveedores`
 --
 
-DROP TABLE IF EXISTS `proveedores`;
 CREATE TABLE IF NOT EXISTS `proveedores` (
-`id` int(10) NOT NULL,
+  `id` int(10) NOT NULL AUTO_INCREMENT,
   `razon` varchar(70) COLLATE utf8_spanish_ci NOT NULL,
   `rif` varchar(15) COLLATE utf8_spanish_ci NOT NULL,
   `direccion` varchar(1000) COLLATE utf8_spanish_ci NOT NULL,
   `telefono` varchar(25) COLLATE utf8_spanish_ci NOT NULL,
   `status` varchar(1) COLLATE utf8_spanish_ci NOT NULL DEFAULT 'A',
   `codigo` varchar(25) COLLATE utf8_spanish_ci NOT NULL,
-  `fecha` date NOT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
+  `fecha` date NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `codigo` (`codigo`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci AUTO_INCREMENT=5 ;
 
 --
 -- Truncar tablas antes de insertar `proveedores`
@@ -211,7 +228,8 @@ TRUNCATE TABLE `proveedores`;
 
 INSERT INTO `proveedores` (`id`, `razon`, `rif`, `direccion`, `telefono`, `status`, `codigo`, `fecha`) VALUES
 (2, 'inversiones', 'J-40190154-9', 'dirección de muestra, calle la cuadradita', '0243-2713131', 'A', '010001', '0000-00-00'),
-(3, 'interiores y fachadas c.a.', 'J-12345678-9', 'Maracay Edo Aragua', '0243-2713140', 'A', '123132', '0000-00-00');
+(3, 'interiores y fachadas c.a.', 'J-12345678-9', 'Maracay Edo Aragua', '0243-2713140', 'A', '123132', '0000-00-00'),
+(4, 'ferreteria El Martillo', 'j404040401', 'el valle, santa rita, maracay estado aragua', '', 'A', '3010', '0000-00-00');
 
 -- --------------------------------------------------------
 
@@ -219,14 +237,15 @@ INSERT INTO `proveedores` (`id`, `razon`, `rif`, `direccion`, `telefono`, `statu
 -- Estructura de tabla para la tabla `usuarios`
 --
 
-DROP TABLE IF EXISTS `usuarios`;
 CREATE TABLE IF NOT EXISTS `usuarios` (
-`id` int(7) NOT NULL,
+  `id` int(7) NOT NULL AUTO_INCREMENT,
   `usuario` varchar(20) COLLATE utf8_spanish_ci NOT NULL,
   `clave` varchar(50) COLLATE utf8_spanish_ci NOT NULL,
   `nombre_completo` varchar(50) COLLATE utf8_spanish_ci NOT NULL,
-  `tipo` varchar(1) COLLATE utf8_spanish_ci NOT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
+  `tipo` varchar(1) COLLATE utf8_spanish_ci NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `usuario` (`usuario`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci AUTO_INCREMENT=4 ;
 
 --
 -- Truncar tablas antes de insertar `usuarios`
@@ -242,91 +261,6 @@ INSERT INTO `usuarios` (`id`, `usuario`, `clave`, `nombre_completo`, `tipo`) VAL
 (3, 'mari', '51c30cf5b566235f70673a8092853fa4b0bb60e4', 'Maricarmen Ochoa', 'C');
 
 --
--- Índices para tablas volcadas
---
-
---
--- Indices de la tabla `categorias`
---
-ALTER TABLE `categorias`
- ADD PRIMARY KEY (`id`), ADD UNIQUE KEY `nombre_categoria` (`nombre_categoria`), ADD KEY `nombre_categoria_2` (`nombre_categoria`);
-
---
--- Indices de la tabla `compras`
---
-ALTER TABLE `compras`
- ADD PRIMARY KEY (`id`), ADD KEY `idProveedor` (`idProveedor`), ADD KEY `idU` (`idU`), ADD KEY `tipo_cuenta` (`tipo_cuenta`);
-
---
--- Indices de la tabla `cuentas`
---
-ALTER TABLE `cuentas`
- ADD PRIMARY KEY (`id`), ADD UNIQUE KEY `nombre` (`nombre`), ADD UNIQUE KEY `codigo` (`codigo`), ADD KEY `usuario` (`usuario`), ADD KEY `usuario_2` (`usuario`);
-
---
--- Indices de la tabla `empresas`
---
-ALTER TABLE `empresas`
- ADD PRIMARY KEY (`id`), ADD UNIQUE KEY `codigo` (`codigo`), ADD KEY `usuario` (`usuario`);
-
---
--- Indices de la tabla `productos`
---
-ALTER TABLE `productos`
- ADD PRIMARY KEY (`id`);
-
---
--- Indices de la tabla `proveedores`
---
-ALTER TABLE `proveedores`
- ADD PRIMARY KEY (`id`), ADD UNIQUE KEY `codigo` (`codigo`);
-
---
--- Indices de la tabla `usuarios`
---
-ALTER TABLE `usuarios`
- ADD PRIMARY KEY (`id`), ADD UNIQUE KEY `usuario` (`usuario`);
-
---
--- AUTO_INCREMENT de las tablas volcadas
---
-
---
--- AUTO_INCREMENT de la tabla `categorias`
---
-ALTER TABLE `categorias`
-MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=13;
---
--- AUTO_INCREMENT de la tabla `compras`
---
-ALTER TABLE `compras`
-MODIFY `id` int(10) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=4;
---
--- AUTO_INCREMENT de la tabla `cuentas`
---
-ALTER TABLE `cuentas`
-MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=9;
---
--- AUTO_INCREMENT de la tabla `empresas`
---
-ALTER TABLE `empresas`
-MODIFY `id` int(10) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=8;
---
--- AUTO_INCREMENT de la tabla `productos`
---
-ALTER TABLE `productos`
-MODIFY `id` int(5) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=11;
---
--- AUTO_INCREMENT de la tabla `proveedores`
---
-ALTER TABLE `proveedores`
-MODIFY `id` int(10) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=4;
---
--- AUTO_INCREMENT de la tabla `usuarios`
---
-ALTER TABLE `usuarios`
-MODIFY `id` int(7) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=4;
---
 -- Restricciones para tablas volcadas
 --
 
@@ -334,19 +268,19 @@ MODIFY `id` int(7) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=4;
 -- Filtros para la tabla `compras`
 --
 ALTER TABLE `compras`
-ADD CONSTRAINT `compras_ibfk_1` FOREIGN KEY (`idProveedor`) REFERENCES `proveedores` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+  ADD CONSTRAINT `compras_ibfk_1` FOREIGN KEY (`idProveedor`) REFERENCES `proveedores` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
 -- Filtros para la tabla `cuentas`
 --
 ALTER TABLE `cuentas`
-ADD CONSTRAINT `enlace_a_usuarios` FOREIGN KEY (`usuario`) REFERENCES `usuarios` (`usuario`) ON DELETE CASCADE ON UPDATE CASCADE;
+  ADD CONSTRAINT `enlace_a_usuarios` FOREIGN KEY (`usuario`) REFERENCES `usuarios` (`usuario`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
 -- Filtros para la tabla `empresas`
 --
 ALTER TABLE `empresas`
-ADD CONSTRAINT `empresas_ibfk_1` FOREIGN KEY (`usuario`) REFERENCES `usuarios` (`usuario`) ON DELETE CASCADE ON UPDATE CASCADE;
+  ADD CONSTRAINT `empresas_ibfk_1` FOREIGN KEY (`usuario`) REFERENCES `usuarios` (`usuario`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
